d36c5ab7e219e38304ebad9a64a8d24ab261d9b6
Jeff McAffer <jeff.mcaffer@microsoft.com>
2016-11-10 23:59:42 -0800

    Break crawler into three parts

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports.eventFinder = require('./lib/eventFinder');
 module.exports.webhookDriver = require('./lib/webhookDriver');
 module.exports.crawler = require('./lib/crawler');
+module.exports.request = require('./lib/request');
+module.exports.processor = require('./lib/processor');
