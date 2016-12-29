8b7dfa269a4fef8c5dc5d38a9e0676ec5b04bc33
Maggie Pint <magpint@microsoft.com>
2016-12-28 16:26:09 -0800

    file processor process file checkpoint

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports.crawlerService = require('./lib/crawlerService');
 module.exports.eventFinder = require('./lib/eventFinder');
 module.exports.githubFetcher = require('./lib/githubFetcher');
 module.exports.githubProcessor = require('./lib/githubProcessor');
+module.exports.fileProcessor = require('./lib/fileProcessor');
 module.exports.policy = require('./lib/traversalPolicy');
 module.exports.queueSet = require('./lib/queueSet');
 module.exports.request = require('./lib/request');
