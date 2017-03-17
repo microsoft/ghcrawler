8a5626064ab3b3d5a1979f600bbf75602e36c0b4
William Bartholomew <wdb@willbar.com>
2017-03-17 13:46:50 -0700

    Add missing copyright headers and remove test file

--- test/integration/mongoDocStore.js
+++
@@ -1,7 +0,0 @@
-const DocStore = require('../../providers/storage/mongodocstore');
-const docStore = new DocStore('mongodb://localhost/foo');
-docStore.connect().then(() => {
-  return docStore.count();
-}).then(count => {
-  console.log(count);
-});
