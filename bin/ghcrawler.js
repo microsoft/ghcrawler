d194f9102d5e593c51d4c664e17b41762d190633
Patrick Steele-Idem <pnidem@gmail.com>
2017-06-28 08:15:14 -0700

    Use Q instead of native promises

--- bin/ghcrawler.js
+++ bin/ghcrawler.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-const start = require('../src').start;
+
 const path = require('path');
+const Q = require('q');
+const start = require('../src').start;
 
 const args = require('argly').createParser({
     '--config -c *': 'string'
   })
   .parse();
 
-Promise.resolve()
+Q()
   .then(function loadOptions() {
     var configFile = args.config || process.env.CRAWLER_CONFIG;
     // Load options
