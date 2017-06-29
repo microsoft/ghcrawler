a05f7f2ed9f034019c7eeae6a57a5d5414f7b72a
Jeff McAffer <jmcaffer@microsoft.com>
2017-06-29 12:39:13 -0700

    Add sample configuration loaders for inMemory and ciab

---
+++ configurations/inmemory.js
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const crawlerDefaults = {
+  name: config.get('CRAWLER_NAME') || 'crawler',
+  count: 1
+};
+
+const fetcherDefaults = {
+  githubTokens: options.githubTokens || config.get('CRAWLER_GITHUB_TOKENS'),
+  callCapStore: 'memory',
+  computeLimitStore: 'memory'
+};
+
+const queuingDefaults = {
+  weights: { events: 10, immediate: 3, soon: 2, normal: 3, later: 2 },
+};
+
+module.exports = function loadConfig() {
+  const services = {
+    logger: createLogger(),
+    configFactory: createInMemoryOptionsFactory()
+  };
+
+  // Instantiate each of the required services and add them to the service list.
+  // Each service module has one or more factory functions that instantiates
+  // the service and addes it to the supplied list of services with the (optionally)
+  // given name.  The service may use or connect to other services supplied in the
+  // given service list. The factory function also returns the newly created service.
+  // Factory functions
+  require('nullLocker').create('locker', services);
+  require('inMemoryStore').create('store', services);
+  require('inMemoryQueues').create('queuing', services, queuingDefaults);
+  require('inMemoryStore').create('deadletters', services);
+  require('githubFetcher').create('fetcher', services, fetcherDefaults);
+  require('githubProcessor').create('processor', services);
+  require('crawler').create('crawler', services, crawlerDefaults);
+  return services;
+}
+
